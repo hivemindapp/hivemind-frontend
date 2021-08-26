3e2462e06bee54a1d2438c7939bb81322c9fc5d1
Kat <kat.white.a@gmail.com>
2021-08-26 16:58:40 -0600

    Add cute bee logo and simple styling for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import bee from '../../images/bee.png'
+import "./Header.css"
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
   return (
     <header>
+      <img src={bee} alt="cute-bee" className="logo"/>
       <h1>HiveMind</h1>
     </header>
   );

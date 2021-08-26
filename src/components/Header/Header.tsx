15e2eec7e47abc54a3677e30977ad8af2a39729d
Kat <kat.white.a@gmail.com>
2021-08-26 17:44:12 -0600

    Resolve PR issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import bee from '../../images/bee.png'
-import "./Header.css"
+import React from "react";
+import bee from "../../images/bee.png";
+import "./Header.css";
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
   return (
     <header>
-      <img src={bee} alt="cute-bee" className="logo"/>
+      <img src={bee} alt="round smiling bee" className="logo" />
       <h1>HiveMind</h1>
     </header>
   );

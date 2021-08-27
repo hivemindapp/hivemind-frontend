7c002d0db31ad876dff983121f1e06e9029c2c37
Kat <kat.white.a@gmail.com>
2021-08-26 18:37:42 -0600

    Set up typescript structure for Posts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
-import { Header } from '../Header/Header';
+import React, { useState, useEffect } from "react";
+import { Header } from "../Header/Header";
+import mockPosts from "../../mockdata/mockPosts.json";
+import { Cards } from "../Cards/Cards";
 
-interface AppProps {}
+interface AppProps {
+  posts: {
+    id: number;
+    attributes: {
+      user_id: number;
+      username: string;
+      title: string;
+      description: string;
+      photo: string;
+      created_at: string;
+      upvotes: number;
+      downvotes: number;
+    };
+  }[];
+}
 
 export const App: React.FC<AppProps> = () => {
-  return <Header />;
+  const [posts, setPosts] = useState<AppProps["posts"]>({});
+
+  useEffect(() => {
+    setPosts(mockPosts.data);
+  }, []);
+
+  return (
+    <main>
+      <Header />
+      <Cards posts={posts} />
+    </main>
+  );
 };
---
+++ src/components/Cards/Cards.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Cards.css";
+
+interface CardsProps {
+  posts: Post[];
+}
+
+interface Post {
+  id: number;
+  attributes: {
+    user_id: number;
+    username: string;
+    title: string;
+    description: string;
+    photo: string;
+    created_at: string;
+    upvotes: number;
+    downvotes: number;
+  };
+}
+
+export const Cards: React.FC<CardsProps> = ({ posts }) => {
+  return <div></div>;
+};

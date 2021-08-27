ab543b307b885e42577c88a6f84c94b77ec39b95
Claire Fields <clairefields15@gmail.com>
2021-08-26 20:03:44 -0600

    Add very basic styling to card and grid to cards section

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './Cards.css';
 import { Post, Posts } from '../App/App';
 import { Card } from '../Card/Card';
+import './Cards.css';
+
 interface CardsProps {
   posts: Posts | {};
 }
@@ -9,10 +10,9 @@ interface CardsProps {
 export const Cards: React.FC<CardsProps> = ({ posts }) => {
   const makeCards = (posts: any) => {
     return posts.map((post: Post) => {
-      console.log(post, 'jhi');
       return <Card key={post.id} post={post} />;
     });
   };
 
-  return <article>{makeCards(posts)}</article>;
+  return <section className='cards-container'>{makeCards(posts)}</section>;
 };

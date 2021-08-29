bc46676b27301e21bf118e55f0f5b9f1fcb48846
Claire Fields <clairefields15@gmail.com>
2021-08-28 18:26:34 -0600

    Move post query out of index and into apiCalls

--- src/index.tsx
+++ src/index.tsx
@@ -3,35 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://hivemind-backend.herokuapp.com/graphql',
   cache: new InMemoryCache()
 });
-
-export const GET_ALL_POSTS = gql`
-  query {
-    posts {
-      id
-      title
-      image
-      upvotes
-      downvotes
-      createdAt
-      user {
-        id
-        username
-        avatar
-      }
-    }
-  }
-`;
 
 ReactDOM.render(
   <ApolloProvider client={client}>
--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -1,1 +1,19 @@
-export {};
+import { gql } from '@apollo/client';
+
+export const GET_ALL_POSTS = gql`
+  query {
+    posts {
+      id
+      title
+      image
+      upvotes
+      downvotes
+      createdAt
+      user {
+        id
+        username
+        avatar
+      }
+    }
+  }
+`;

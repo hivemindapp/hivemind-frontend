6b099714caa25af6fe24a7b7f0db70385d0b90d4
Kat <kat.white.a@gmail.com>
2021-09-01 20:02:44 -0600

    Fix merge conflicts

--- cypress/integration/modal_spec.js
+++ cypress/integration/modal_spec.js
@@ -1,5 +1,15 @@
+import { aliasPostsQuery } from '../utils/graphql-test-utils';
+
 describe('Modal Spec', () => {
   beforeEach(() => {
+    cy.intercept(
+      'POST',
+      'https://hivemind-staging-branch.herokuapp.com/graphql',
+      (req) => {
+        aliasPostsQuery(req, 'posts');
+      }
+    );
+
     cy.visit('http://localhost:3000/').get('.add-post-btn').click();
   });
 

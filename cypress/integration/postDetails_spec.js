069e912cab97d415c520e8533aa0b2bd62a4e9eb
Kat <kat.white.a@gmail.com>
2021-09-06 17:57:40 -0600

    Add bones of tests

--- cypress/integration/postDetails_spec.js
+++ cypress/integration/postDetails_spec.js
@@ -12,4 +12,12 @@ describe('Modal Spec', () => {
 
     cy.visit('http://localhost:3000/').get('.card').first();
   });
+
+  it('Should have a title', () => {});
+
+  it('Should display the original poster, with posters avatar, and the time of posting.', () => {});
+
+  it('Should display the description', () => {});
+
+  it('Should display the image (if one is available)', () => {});
 });
